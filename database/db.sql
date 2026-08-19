a46bf1ed6f1ad58f5aac30176dc4dd74430a1a17
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-08-19 08:37:16 -0300

    fix - sql consertado para a inserção na tabela

--- database/db.sql
+++ database/db.sql
@@ -1,17 +1,18 @@
-CREATE DATABASE pratos_m1;
+CREATE DATABASE IF NOT EXISTS pratos_m1;
 USE pratos_m1;
 
-CREATE TABLE pratos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(200) NOT NULL,
-    descricao VARCHAR(100)NOT NULL,
-    preco INT NOT NULL,
-    categoria VARCHAR(100) NOT NULL,
-    FOREIGN KEY ('nomeusuario')
-
-);
-CREATE TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nomeusuario VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pratos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    nome VARCHAR(200) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    CONSTRAINT fk_pratos_usuarios FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
